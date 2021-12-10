1f8b597bb674f79f6994108d553ca3226505b62f
LucasPickering <LucasPickering@users.noreply.github.com>
2021-12-10 22:52:13 +0000

    Deploying to gh-pages from @ LucasPickering/osrs-cli@a6c618dc878afa2b223e21c0157bede5666c2b27 🚀

--- build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    if version_check::is_feature_flaggable().unwrap_or(false) {
-        println!("cargo:rustc-cfg=nightly");
-    }
-}
